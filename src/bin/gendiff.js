09eb3bc1f6efea4536d7e67d7dcea37ec9c99a04
Viacheslav <syva80@Viacheslavs-MacBook-Pro.local>
2019-10-21 02:11:53 +0300

    project2_v0.10

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,9 +1,20 @@
 #!/usr/bin/env node
 
+import compareFile from '..';
+
 const program = require('commander');
 
-program
-  .version('0.0.1', '-v, --version', 'output the current version')
-  .description('Compares two configuration files and shows a difference.')
-  .parse(process.argv);
-console.log('new project!');
+const gendiff = () => {
+  program
+    .version('0.0.1', '-v, --version', 'output the current version')
+    .option('-f, --format [type]', 'Output format', 'json')
+    .description('Compares two configuration files and shows a difference.')
+    .arguments('<firstConfig> <secondConfig>')
+    .action((firstConfig, secondConfig) => {
+      compareFile(firstConfig, secondConfig);
+    });
+  program.parse(process.argv);
+  console.log('new project!');
+};
+gendiff();
+export default { gendiff };
---
+++ src/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const _ = require('lodash');
+
+export default (pathFile1, pathFile2) => {
+  const objBefore = JSON.parse(fs.readFileSync(pathFile1, 'utf8'));
+  const objAfter = JSON.parse(fs.readFileSync(pathFile2, 'utf8'));
+  const str = Object.entries(objBefore).reduce((acc, [key, value]) => {
+    if (_.has(objAfter, key)) {
+      if (value === objAfter[key]) {
+        return `${acc} ${key}: ${value}`;
+      }
+      return `${acc}\n-${key}: ${value}\n+${key}: ${objAfter[key]}`;
+    }
+    return `${acc}\n-${key}: ${value}`;
+  }, '');
+  const resultStr = Object.entries(objAfter)
+    .reduce((acc, [key, value]) => ((_.has(objBefore, key)) ? acc : `${acc}\n+${key}: ${value}`), str);
+  console.log(resultStr);
+};
