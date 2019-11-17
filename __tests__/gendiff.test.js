159ff8e56f711f0cb23f8c29f5545735c69bd4ab
Viacheslav <syva80@Viacheslavs-MacBook-Pro.local>
2019-11-17 20:21:51 +0300

    project_v1.4.6

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,19 +1,24 @@
+import path from 'path';
 import fs from 'fs';
 import gendiff from '../src';
 
-const path = './__fixtures__/';
-const text = fs.readFileSync(`${path}SimpleTextDifferences`, 'utf8');
-const textDeep = fs.readFileSync(`${path}DeepTextDifferences`, 'utf8');
-const textPlain = fs.readFileSync(`${path}PlainTextDifferences`, 'utf8');
-const textJson = fs.readFileSync(`${path}JsonTextDifferences`, 'utf8');
-test.each([[`${path}before.json`, `${path}after.json`, text],
-  [`${path}before.yml`, `${path}after.yml`, text],
-  [`${path}before.ini`, `${path}after.ini`, text],
-  [`${path}beforeDeep.json`, `${path}afterDeep.json`, textDeep],
-])('gendiff', (pathFile1, pathFile2, textResult) => {
-  expect(gendiff(pathFile1, pathFile2)).toEqual(textResult);
+const name = 'SimpleTextDifferences';
+
+const getFixturePath = (nameFile) => path.join(__dirname, '..', '__fixtures__', nameFile);
+const getResult = (nameFile) => fs.readFileSync(getFixturePath(nameFile), 'utf8');
+
+test.each([['before.json', 'after.json', name],
+  ['before.yml', 'after.yml', name],
+  ['before.ini', 'after.ini', name],
+  ['beforeDeep.json', 'afterDeep.json', 'DeepTextDifferences'],
+])('gendiff', (fileBefore, fileAfter, fileName) => {
+  const filePathBefore = getFixturePath(fileBefore);
+  const filePathAfter = getFixturePath(fileAfter);
+  expect(gendiff(filePathBefore, filePathAfter)).toEqual(getResult(fileName));
 });
-test.each([['plain', textPlain], ['json', textJson],
-])('gendiff_output_plain_and_json', (format, textResult) => {
-  expect(gendiff(`${path}before.json`, `${path}after.json`, format)).toEqual(textResult);
+test.each([['plain', 'PlainTextDifferences'], ['json', 'JsonTextDifferences'],
+])('gendiff_output_plain_and_json', (format, fileName) => {
+  const pathBefore = getFixturePath('before.json');
+  const pathAfter = getFixturePath('after.json');
+  expect(gendiff(pathBefore, pathAfter, format)).toEqual(getResult(fileName));
 });
