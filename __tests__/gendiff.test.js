94b65e75844736e36d6797eaf005c227d5c6fdc1
Viacheslav <syva80@Viacheslavs-MacBook-Pro.local>
2019-11-27 12:12:57 +0300

    project2_v1.5.3

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,14 +2,14 @@ import path from 'path';
 import fs from 'fs';
 import gendiff from '../src';
 
-const name = 'SimpleTextDifferences';
+const name = 'DeepTextDifferences';
 
 const getFixturePath = (nameFile) => path.join(__dirname, '..', '__fixtures__', nameFile);
 const getResult = (nameFile) => fs.readFileSync(getFixturePath(nameFile), 'utf8').trim();
 
-test.each([['before.yml', 'after.yml', name],
-  ['before.ini', 'after.ini', name],
-  ['beforeDeep.json', 'afterDeep.json', 'DeepTextDifferences'],
+test.each([['beforeDeep.yml', 'afterDeep.yml', name],
+  ['beforeDeep.ini', 'afterDeep.ini', name],
+  ['beforeDeep.json', 'afterDeep.json', name],
 ])('gendiff', (fileBefore, fileAfter, fileResult) => {
   const filePathBefore = getFixturePath(fileBefore);
   const filePathAfter = getFixturePath(fileAfter);
